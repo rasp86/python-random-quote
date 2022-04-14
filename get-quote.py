6d31706eac8c79150e1372de33bbcac0a8bb3215
rasp86 <45322586+rasp86@users.noreply.github.com>
2022-04-14 10:01:15 +0100

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,4 @@
-def main():
+def primary():
   print("Keep it logically awesome.")
 
   #f = open("quotes.txt")
@@ -7,4 +7,4 @@ def main():
   #print(quotes)
 
 if __name__== "__main__":
-  main()
+  primary()
