06a13f973c0edca711f2640fefcb04e9c1ff3fee
rasp86 <m47m3n@gmail.com>
2022-04-14 10:09:45 +0100

    read firs el from array

--- get-quote.py
+++ get-quote.py
@@ -1,10 +1,10 @@
-def main():
-  print("Keep it logically awesome.")
+def primary():
+  #print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-  #print(quotes)
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  print(quotes)
 
 if __name__== "__main__":
-  main()
+  primary()
