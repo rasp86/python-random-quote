8610a03a080d3470ce63b002b4ba09782068fa0e
rasp86 <m47m3n@gmail.com>
2022-04-14 09:48:45 +0100

    Hello World

--- get-quote.py
+++ get-quote.py
@@ -1,10 +1,9 @@
 def main():
-  # print("Keep it logically awesome.")
+  print("Keep it logically awesome.")
 
   #f = open("quotes.txt")
   #quotes = f.readlines()
   #f.close()
-
   #print(quotes)
 
 if __name__== "__main__":
